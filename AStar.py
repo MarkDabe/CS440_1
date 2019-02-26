7e42d599acf9e273671c2a872a488b3cc42a0818
markhabashy <markhabashy91@gmail.com>
2019-02-25 20:31:54 -0500

    added tie breaking and improved the effect of the algorithm

--- AStar.py
+++ AStar.py
@@ -1,12 +1,12 @@
 from heapq import *
 from random import shuffle
+import pygame
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
-BLUE = (135, 206, 235)
 GREY = (211, 211, 211)
 
 WIDTH = 5
@@ -27,10 +27,7 @@ class AStar:
         self.h = {}
         self.g = {}
         self.closed_list = set()
-        self.visited = set()
         self.blocked = set()
-        self.distance = None
-        self.time = None
         self.grid = []
 
     def re_init(self):
@@ -38,10 +35,7 @@ class AStar:
         self.g = {}
         self.h = {}
         self.closed_list = set()
-        self.visited = set()
         self.blocked = set()
-        self.distance = None
-        self.time = None
         self.grid = []
 
     @classmethod
@@ -64,7 +58,32 @@ class AStar:
         heappush(open_list, (f_score[start], start))
 
         while open_list:
-            current = heappop(open_list)[1]
+
+            smallest_f = open_list[0][0]
+
+            pointer = 0
+
+            tie_breaking_list = []
+
+            list_len = len(open_list)
+
+            while pointer < list_len and open_list[pointer][0] == smallest_f :
+                temp = open_list[pointer]
+                # temp2 = (101 * temp[0] - g_score[temp[1]], temp[1])
+                temp2 = (101 * temp[0] + g_score[temp[1]], temp[1])
+                heappush(tie_breaking_list, temp2)
+                pointer += 1
+            current = heappop(tie_breaking_list)
+            pointer = 0
+            while True:
+                if open_list[pointer][1] == current[1]:
+                    open_list.remove(open_list[pointer])
+                    break
+                pointer +=1
+            heapify(open_list)
+            current = current[1]
+
+            # current = heappop(open_list)[1]
 
             if current == self.goal_state:
                 total_path = []
@@ -75,10 +94,11 @@ class AStar:
                 total_path.append(start)
                 for node in closed_list:
                     self.h[node] = gd - g_score[node]
-                self.h[self.goal_state] = gd - g_score[self.goal_state]
+                self.h[self.goal_state] = 0
                 return total_path
 
             closed_list.add(current)
+            self.closed_list.add(current)
 
             # shuffle(NEIGHBOURS)
             for i, j in NEIGHBOURS:
@@ -94,10 +114,14 @@ class AStar:
 
                     neighbor_previous_g_score = g_score.get(neighbor, 0)
 
-                    if (neighbor in closed_list) and (neighbour_g_score >= neighbor_previous_g_score):
+                    # if (neighbor in closed_list) and (neighbour_g_score >= neighbor_previous_g_score):
+                    #     continue
+
+                    if neighbor in closed_list:
                         continue
 
-                    if (neighbour_g_score < neighbor_previous_g_score) or (neighbor not in [i[1] for i in open_list]):
+                    # if (neighbour_g_score < neighbor_previous_g_score) or (neighbor not in [i[1] for i in open_list]):
+                    if neighbour_g_score < neighbor_previous_g_score or neighbor_previous_g_score == 0:
                         if neighbour_g_score < neighbor_previous_g_score:
                             open_list.remove((f_score[neighbor], neighbor))
                             heapify(open_list)
@@ -107,13 +131,8 @@ class AStar:
 
                         h_score[neighbor] = self.h.get(neighbor, AStar.manhattan_distance(neighbor, self.goal_state))
 
-                        # if neighbor in self.closed_list:
-                        #     h_score[neighbor] = self.h[neighbor]
-                        # else:
-                        #     h_score[neighbor] = AStar.manhattan_distance(neighbor, self.goal_state)
-
                         f_score[neighbor] = neighbour_g_score + h_score[neighbor]
-                        self.closed_list.add(neighbor)
+                        # self.closed_list.add(neighbor)
                         heappush(open_list, (f_score[neighbor], neighbor))
         return False
 
@@ -151,7 +170,31 @@ class AStar:
 
         while open_list:
 
-            current = heappop(open_list)[1]
+            smallest_f = open_list[0][0]
+
+            pointer = 0
+
+            tie_breaking_list = []
+
+            list_len = len(open_list)
+
+            while pointer < list_len and open_list[pointer][0] == smallest_f :
+                temp = open_list[pointer]
+                # temp2 = (101 * temp[0] - g_score[temp[1]], temp[1])
+                temp2 = (101 * temp[0] + g_score[temp[1]], temp[1])
+                heappush(tie_breaking_list, temp2)
+                pointer += 1
+            current = heappop(tie_breaking_list)
+            pointer = 0
+            while True:
+                if open_list[pointer][1] == current[1]:
+                    open_list.remove(open_list[pointer])
+                    break
+                pointer += 1
+            heapify(open_list)
+            current = current[1]
+
+            # current = heappop(open_list)[1]
 
             if ((state == "forward" and current == self.goal_state) or
                 (state == "backward" and current == start)):
@@ -168,6 +211,7 @@ class AStar:
                 return total_path
 
             closed_list.add(current)
+            self.closed_list.add(current)
 
             # shuffle(NEIGHBOURS)
             for i, j in NEIGHBOURS:
@@ -181,13 +225,18 @@ class AStar:
                 else:
                     neighbour_g_score = g_score[current] + 1
 
-                    tentative_previous_g_score = g_score.get(neighbor, 0)
+                    neighbor_previous_g_score = g_score.get(neighbor, 0)
 
-                    if (neighbor in closed_list) and (neighbour_g_score >= tentative_previous_g_score):
+                    # if (neighbor in closed_list) and (neighbour_g_score >= tentative_previous_g_score):
+                    #     continue
+
+                    if neighbor in closed_list:
                         continue
 
-                    if (neighbour_g_score < tentative_previous_g_score) or (neighbor not in [i[1] for i in open_list]):
-                        if neighbour_g_score < tentative_previous_g_score:
+                    # if (neighbour_g_score < tentative_previous_g_score) or (neighbor not in [i[1] for i in open_list]):
+
+                    if neighbour_g_score < neighbor_previous_g_score or neighbor_previous_g_score == 0:
+                        if neighbour_g_score < neighbor_previous_g_score:
                             open_list.remove((f_score[neighbor], neighbor))
                             heapify(open_list)
 
@@ -205,7 +254,8 @@ class AStar:
                             h_score[neighbor] = AStar.manhattan_distance(neighbor, start)
 
                             f_score[neighbor] = neighbour_g_score + h_score[neighbor]
-                        self.closed_list.add(neighbor)
+
+                        # self.closed_list.add(neighbor)
                         heappush(open_list, (f_score[neighbor], neighbor))
         return False
 
@@ -231,6 +281,7 @@ class AStar:
 
             if state == "forward" or state == "adaptive":
                 path.reverse()
+
             for node in path:
                 if array[node[0]][node[1]] == 1:
                     blocked_index = path.index(node)
